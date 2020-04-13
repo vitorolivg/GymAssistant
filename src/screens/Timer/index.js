c960e28a06d107eb9dcf1c6849c10e377ac9e818
Gustavo Garcia <gustavocodess>
2020-04-13 01:06:17 -0300

    inital commit with react navigation configuration and screens

---
+++ src/screens/Timer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function Timer() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Timer here!</Text>
+    </View>
+  )
+}
+
+export default Timer
