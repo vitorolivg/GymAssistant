9465bb004188fd16bd415d8183eb8009fa6597e9
Gustavo Garcia <gustavocodess>
2020-04-16 23:33:49 -0300

    fix for jest test adding jest.useFakeTimers

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -6,6 +6,10 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 
+// added the below line for test passing
+// accordingly to https://github.com/facebook/jest/issues/6434#issuecomment-525576660
+jest.useFakeTimers()
+
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
